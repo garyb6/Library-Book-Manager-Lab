b4c9a12043b2c456db76e054ccc548162514be2a
garyb6 <gary.bennett6@outlook.com>
2021-12-08 15:27:34 +0000

    wrote some functions

--- repositories/author_repository.py
+++ repositories/author_repository.py
@@ -4,27 +4,55 @@ from models.author import Author
 from models.book import Book
 
 
-def save():
-   pass
+def save(author):
+    sql = "INSERT INTO authors (name) VALUES (%s) RETURNING *"
+    values = [author.name]
+    results = run_sql (sql, values)
+    id = results[0]['id']
+    author.id = id 
+    return author 
 
 
 def select_all():
-    pass
+    authors = []
+    sql = "SELECT * FROM authors"
+    results = run_sql(sql)
+    for row in results:
+        author = Author(row["name"], row["id"])
+        authors.append(author)
+    return authors 
 
-
-def select():
-    pass
+def select(id):
+    author = None
+    sql = "SELECT * FROM authors WHERE id= %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+    if result is not None:
+        author = Author(result["name"], result["id"])
+    return author 
 
 def delete_all():
-    pass
+    sql = "DELETE FROM authors"
+    run_sql(sql)
 
 
-def delete():
-    pass
+def delete(id):
+    sql = "DELETE FROM authors WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
 
 
-def update():
-    pass
+def update(author):
+    sql = "UPDATE authors SET (name) = (%s) WHERE id = %s"
+    values = [author.name, author.id]
+    run_sql(sql, values)
 
-def books():
-    pass
+def books(author):
+    books = []
+    sql = "SELECT * FROM books WHERE author_id = %s"
+    values= [author.id]
+    results = run_sql(sql, values)
+    for row in results:
+        book = Book(row['title'], row['author_id'], row['id'])
+        books.append(book)
+    return books 
